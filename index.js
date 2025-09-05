d387920cba90850087245e6df73143c9a7753b11
Era-smus <erasmussaat1@gmail.com>
2025-09-06 00:39:38 +0300

    Modified index.htlm

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'john'
+function changeLeastFavoriteCustomer() {
+leastFavoriteCustomer = 'mary';
+}
+
 // Write your solution in this file!
+   
